eadb42e85ea59b9270eec689357474af03b11ca0
BobJoeman21 <ccwhitford18@gmail.com>
2025-10-19 17:09:12 -0400

    FPS update
    Set FPS to 144

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -13,7 +13,7 @@ public class PlayerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Application.targetFrameRate = 60;
+        Application.targetFrameRate = 144;
     }
 
     // Update is called once per frame
