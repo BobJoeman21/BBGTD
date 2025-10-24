67f6c12f6f5f981056577d9c7127813e79238d79
BobJoeman21 <ccwhitford18@gmail.com>
2025-10-24 14:27:22 -0400

    Art, Animations

--- Assets/Scripts/ProducerTowerScript.cs
+++ Assets/Scripts/ProducerTowerScript.cs
@@ -8,19 +8,17 @@ public class ProducerTowerScript : MonoBehaviour
     public GameObject GrudgePrefab;
     public int ProduceTimerMax;
     private int ProduceTimerCurrent;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    
 
-    // Update is called once per frame
+
     void FixedUpdate()
     {
         ProduceTimerCurrent += 1;
         if(ProduceTimerCurrent >= ProduceTimerMax)
         {
             //SpawnGrudge
+            GetComponentInChildren<Animator>().SetTrigger("Spawn");
             Instantiate(GrudgePrefab, new Vector3((transform.position.x + Random.Range(-.2f, .3f)),gameObject.transform.position.y), Quaternion.identity);
             ProduceTimerCurrent = 0;
         }
